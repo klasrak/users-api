da0397d8c03d5f3a44063deaf671cfd47960c4fa
Danilo Augusto <dasf_cm@terra.com.br>
2021-09-16 00:10:01 -0300

    feat: add gin and godotenv init config

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	log.Println("Starting server...")
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Graceful server shutdown - https://github.com/gin-gonic/examples/blob/master/graceful-shutdown/graceful-shutdown/notify-without-context/server.go
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", srv.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal, 2)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// This blocks until a signal is passed into the quit channel
+	<-quit
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
 }
