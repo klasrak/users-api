0e47e13123172f8a32f844f9707e582500208198
Danilo Augusto <dasf_cm@terra.com.br>
2021-09-17 11:05:22 -0300

    refactor: edit entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,7 +6,7 @@ arg="$1"
 HOST="${arg%%:*}"
 PORT="${arg#*:}"
 
-until nc -w 1 -z ${HOST} ${PORT}; do
+until docker-compose exec postgres sh -c 'pg_isready'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
