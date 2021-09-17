6553a6f99ed9a626daa7c0ba4ac3bb937c2147c8
Danilo Augusto <dasf_cm@terra.com.br>
2021-09-16 21:54:49 -0300

    feat: add user repository mocks

--- go.mod
+++ go.mod
@@ -33,5 +33,6 @@ require (
 	github.com/jmoiron/sqlx v1.3.4 // indirect
 	github.com/lib/pq v1.10.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ mocks/mock_user_repository.go
@@ -0,0 +1,91 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	model "github.com/klasrak/users-api/models"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockUserRepository is a mock type for service.UserRepository interface
+type MockUserRepository struct {
+	mock.Mock
+}
+
+// GetAll is a mock for UserRepository GetAll
+func (m *MockUserRepository) GetAll(ctx context.Context) ([]*model.User, error) {
+	ret := m.Called(ctx)
+
+	var r0 []*model.User
+
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).([]*model.User)
+	}
+
+	var r1 error
+
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
+}
+
+// GetByID is a mock for UserRepository GetByID
+func (m *MockUserRepository) GetByID(ctx context.Context, id uuid.UUID) (*model.User, error) {
+	ret := m.Called(ctx, id)
+
+	var r0 *model.User
+
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(*model.User)
+	}
+
+	var r1 error
+
+	if ret.Get(1) != nil {
+		r1 = ret.Get(1).(error)
+	}
+
+	return r0, r1
+}
+
+// Create is a mock for UserRepository Create
+func (m *MockUserRepository) Create(ctx context.Context, u *model.User) error {
+	ret := m.Called(ctx, u)
+
+	var r0 error
+
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(error)
+	}
+
+	return r0
+}
+
+// Update is a mock for UserRepository Update
+func (m *MockUserRepository) Update(ctx context.Context, u *model.User) error {
+	ret := m.Called(ctx, u)
+
+	var r0 error
+
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(error)
+	}
+
+	return r0
+}
+
+// Delete is a mock for UserRepository Delete
+func (m *MockUserRepository) Delete(ctx context.Context, id uuid.UUID) error {
+	ret := m.Called(ctx, id)
+
+	var r0 error
+
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(error)
+	}
+
+	return r0
+}
