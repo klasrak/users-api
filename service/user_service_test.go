6337c904fff9b27c93b9972cc7da3a260bef3b3d
Danilo Augusto <dasf_cm@terra.com.br>
2021-09-18 19:19:00 -0300

    test: add userService GetAll with name filter test

--- service/user_service_test.go
+++ service/user_service_test.go
@@ -3,9 +3,11 @@ package service
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/bxcodec/faker/v3"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/klasrak/users-api/mocks"
 	model "github.com/klasrak/users-api/models"
 	"github.com/stretchr/testify/assert"
@@ -39,5 +41,35 @@ func TestUserService(t *testing.T) {
 
 			mockUserRepository.AssertExpectations(t)
 		})
+	})
+
+	t.Run("Success with name filter", func(t *testing.T) {
+		var users []model.User
+
+		user := model.User{
+			UID:       uuid.New(),
+			Name:      "John Doe",
+			Email:     faker.Email(),
+			Cpf:       "123.456.789-10",
+			BirthDate: time.Now(),
+		}
+
+		users = append(users, user)
+
+		mockUserRepository := new(mocks.MockUserRepository)
+		mockUserRepository.On("GetAll", mock.AnythingOfType("*context.emptyCtx"), mock.AnythingOfType("string")).Return(users, nil)
+
+		userService := &UserService{
+			UserRepository: mockUserRepository,
+		}
+
+		ctx := context.Background()
+
+		us, err := userService.GetAll(ctx, "John")
+
+		assert.NoError(t, err)
+		assert.Equal(t, users, us)
+
+		mockUserRepository.AssertExpectations(t)
 	})
 }
