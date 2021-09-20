8634eb104cc446eb7c8724a93db585cea75462e1
Danilo Augusto <dasfcm@gmail.com>
2021-09-20 03:06:47 -0300

    test: add user repository GetByID tests

--- repository/pg_user_repository_test.go
+++ repository/pg_user_repository_test.go
@@ -129,4 +129,64 @@ func TestUserRepository(t *testing.T) {
 			assert.Len(t, users, 0)
 		})
 	})
+
+	t.Run("GetByID", func(t *testing.T) {
+		t.Run("Success", func(t *testing.T) {
+			uid, _ := uuid.NewRandom()
+			u := &model.User{
+				UID:       uid,
+				Name:      faker.Name(),
+				Email:     faker.Email(),
+				Cpf:       "313.716.772-80",
+				BirthDate: time.Date(1990, 1, 1, 1, 1, 1, 1, time.UTC),
+			}
+			db, mock := NewMock()
+
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+			defer sqlxDB.Close()
+
+			query := `SELECT \* FROM users WHERE id\=\$1;`
+
+			userRepository := &UserRepository{DB: sqlxDB}
+
+			rows := sqlmock.NewRows([]string{"id", "name", "email", "cpf", "birthdate"}).AddRow(u.UID, u.Name, u.Email, u.Cpf, u.BirthDate)
+
+			mock.ExpectQuery(query).WithArgs(u.UID).WillReturnRows(rows)
+
+			ctx := context.Background()
+
+			user, err := userRepository.GetByID(ctx, uid)
+
+			assert.NoError(t, err)
+			assert.NotNil(t, user)
+			assert.Equal(t, u, user)
+		})
+
+		t.Run("Error", func(t *testing.T) {
+			uid, _ := uuid.NewRandom()
+			u := &model.User{}
+			db, mock := NewMock()
+
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+			defer sqlxDB.Close()
+
+			query := `SELECT \* FROM users WHERE id\=\$1;`
+
+			userRepository := &UserRepository{DB: sqlxDB}
+
+			mock.ExpectQuery(query).WithArgs(uid).WillReturnError(sql.ErrNoRows)
+
+			ctx := context.Background()
+
+			user, err := userRepository.GetByID(ctx, uid)
+
+			assert.Error(t, err)
+			assert.NotNil(t, user)
+			assert.Equal(t, rerrors.NewNotFound("id", uid.String()), err)
+			assert.Equal(t, u, user)
+		})
+
+	})
 }
