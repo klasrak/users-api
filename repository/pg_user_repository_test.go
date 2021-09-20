6e50e873d2fdfcaa83af631749b1b90081797347
Danilo Augusto <dasfcm@gmail.com>
2021-09-20 02:46:42 -0300

    test: add user repository GetAll success test

--- repository/pg_user_repository_test.go
+++ repository/pg_user_repository_test.go
@@ -1,12 +1,19 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/bxcodec/faker/v3"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	model "github.com/klasrak/users-api/models"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 )
 
 func NewMock() (*sql.DB, sqlmock.Sqlmock) {
@@ -21,19 +28,36 @@ func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 func TestUserRepository(t *testing.T) {
 	t.Run("GetAll", func(t *testing.T) {
 		t.Run("Success without name filter", func(t *testing.T) {
-			// db, mock := NewMock()
+			uid, _ := uuid.NewRandom()
+			u := &model.User{
+				UID:       uid,
+				Name:      faker.Name(),
+				Email:     faker.Email(),
+				Cpf:       "313.716.772-80",
+				BirthDate: time.Date(1990, 1, 1, 1, 1, 1, 1, time.UTC),
+			}
+			db, mock := NewMock()
 
-			// sqlxDB := sqlx.NewDb(db, "sqlmock")
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
 
-			// defer sqlxDB.Close()
+			defer sqlxDB.Close()
 
-			// userRepository := &UserRepository{DB: sqlxDB}
+			query := `SELECT \* FROM users u;`
 
-			// var users []model.User
+			userRepository := &UserRepository{DB: sqlxDB}
 
-			// err := faker.FakeData(&users)
+			rows := sqlmock.NewRows([]string{"id", "name", "email", "cpf", "birthdate"}).AddRow(u.UID, u.Name, u.Email, u.Cpf, u.BirthDate)
 
-			// assert.NoError(t, err)
+			mock.ExpectQuery(query).WillReturnRows(rows)
+
+			ctx := context.Background()
+
+			users, err := userRepository.GetAll(ctx, "")
+
+			assert.NotNil(t, users)
+			assert.NoError(t, err)
+			assert.IsType(t, []model.User{}, users)
+			assert.Len(t, users, 1)
 		})
 	})
 }
