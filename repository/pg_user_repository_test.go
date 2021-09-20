b9531bd490c4298ac0780e99b790a49053400332
Danilo Augusto <dasfcm@gmail.com>
2021-09-20 02:48:39 -0300

    test: add user repository GetAll with filter test

--- repository/pg_user_repository_test.go
+++ repository/pg_user_repository_test.go
@@ -59,5 +59,38 @@ func TestUserRepository(t *testing.T) {
 			assert.IsType(t, []model.User{}, users)
 			assert.Len(t, users, 1)
 		})
+
+		t.Run("Success with name filter", func(t *testing.T) {
+			uid, _ := uuid.NewRandom()
+			u := &model.User{
+				UID:       uid,
+				Name:      faker.Name(),
+				Email:     faker.Email(),
+				Cpf:       "313.716.772-80",
+				BirthDate: time.Date(1990, 1, 1, 1, 1, 1, 1, time.UTC),
+			}
+			db, mock := NewMock()
+
+			sqlxDB := sqlx.NewDb(db, "sqlmock")
+
+			defer sqlxDB.Close()
+
+			query := `SELECT \* FROM users u;`
+
+			userRepository := &UserRepository{DB: sqlxDB}
+
+			rows := sqlmock.NewRows([]string{"id", "name", "email", "cpf", "birthdate"}).AddRow(u.UID, u.Name, u.Email, u.Cpf, u.BirthDate)
+
+			mock.ExpectQuery(query).WillReturnRows(rows)
+
+			ctx := context.Background()
+
+			users, err := userRepository.GetAll(ctx, u.Name)
+
+			assert.NotNil(t, users)
+			assert.NoError(t, err)
+			assert.IsType(t, []model.User{}, users)
+			assert.Len(t, users, 1)
+		})
 	})
 }
